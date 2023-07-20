e22642a50170283e41ad28fe863660a0837f8126
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2023-07-20 19:55:43 -0400

    Create FooterComponent.js

---
+++ fantasyvolleyball/components/FooterComponent.js
@@ -0,0 +1,14 @@
+export default{
+  template:`
+  <footer class="mt-4" id="footer">
+      <div class="social-media-buttons">
+        <a href="https://www.instagram.com/fantasyvball" class="social-media-button"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://www.threads.net/@fantasyvball" class="social-media-button"><i class="fas fa-at"></i></a>
+        <a href="https://www.instagram.com/fantasyvball" class="social-media-button"><i class="fab fa-instagram"></i></a>
+      </div>
+      <div class="copy-right-text">
+        &copy; 2023 Fantasy Volleyball. All rights reserved.
+      </div>
+    </footer>
+    `,
+}
