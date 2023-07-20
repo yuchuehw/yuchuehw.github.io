2dad4b71b848147f8cba042657063e1c64eaa4e1
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2023-07-20 19:59:50 -0400

    Delete my-component.js

--- fantasyvolleyball/components/my-component.js
+++
@@ -1,8 +0,0 @@
-import { ref } from 'vue'
-export default {
-  setup() {
-    const count = ref(0)
-    return { count }
-  },
-  template: `<button @click="count++">Count is: {{ count }}</button>`
-}
