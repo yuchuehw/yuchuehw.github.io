8864f26e74efef6d53fd97477f35db0b6772663d
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-06-07 15:04:01 -0700

    Create script.js

---
+++ booksdb/script.js
@@ -0,0 +1,8 @@
+let config = {
+  locateFile: () => "./sql/sql-wasm.wasm",
+};
+const sqlPromise = initSqlJs(config);
+const dataPromise = fetch("./db/dummy_dataset.db").then(res => res.arrayBuffer());
+const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
+const db = new SQL.Database(new Uint8Array(buf));
+export default db;
